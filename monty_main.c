15a67dac4deaae09fd0e867977480de9ac130e4f
Rawana683 <118109121+Rawana683@users.noreply.github.com>
2023-03-25 15:15:17 +0200

    Update monty_main.c

--- monty_main.c
+++ monty_main.c
@@ -1,34 +1,51 @@
 #include "monty.h"
+#include <stdio.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
+
+bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
-* free_stack - function that frees a doubly linked list
-* @head: head pointer to the stack
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
 *
-* Return: nothing
+* Return: 0 on success
 */
-void free_stack(stack_t *head)
+int main(int argc, char *argv[])
 {
-	stack_t *temp;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	temp = head;
-	while (head)
+	if (argc != 2)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-}
-
-/**
-* f_stack - function that prints the top
-* @head: head of stack
-* @counter: line count
-*
-* Return: nothing
-*/
-void f_stack(stack_t **head, unsigned int counter)
-{
-	(void)head;
-	(void)counter;
-	bus.lifi = 0;
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
